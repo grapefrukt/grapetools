3b1012e562c4df6a94d8e6aa73529010a1250dd6
Martin Jonasson <grapefrukt@grapefrukt.com>
2014-04-23 14:29:44 +0200

    Double cooldown now has a toggle for wether to automatically start the second cooldown when the first completes

--- com/grapefrukt/utils/DoubleCooldown.hx
+++ com/grapefrukt/utils/DoubleCooldown.hx
@@ -10,22 +10,46 @@ class DoubleCooldown {
 	public var first(default, null):Cooldown;
 	public var second(default, null):Cooldown;
 	
+	public var inFirst(get, never):Bool;
+	public var inSecond(get, never):Bool;
+	public var phaseComplete(get, never):Bool;
+	
+	private var current:Cooldown;
+	
+	public var autoTogglePhase:Bool = true;
+	
 	public function new(firstDuration:Float, secondDuration:Float, onCompleteFirst:Void->Void = null, onCompleteSecond:Void->Void = null) {
 		first = new Cooldown(firstDuration, false, onCompleteFirst);
 		second = new Cooldown(secondDuration, false, onCompleteSecond);
+		
+		current = first;
 	}
 	
 	public function update(dt:Float) {
-		if (!first.isCompleted) {
-			first.update(dt);
-		} else if (!second.isCompleted) {
-			second.update(dt);
-		} else {
-			first.reset();
-			second.reset();
-			
-			first.update(dt);
+		if (!current.isCompleted) {
+			current.update(dt);
+		} else if (autoTogglePhase) {
+			togglePhase();			
+			current.update(dt);
 		}
+	}
+	
+	public function togglePhase() {
+		current.reset();
+		// toggle the current cooldown
+		current = (current == first) ? second : first;
+	}
+	
+	public function get_phaseComplete():Bool {
+		return current.isCompleted;
+	}
+	
+	private function get_inFirst():Bool {
+		return current == first;
+	}
+	
+	private function get_inSecond():Bool {
+		return current == second;
 	}
 	
 }
